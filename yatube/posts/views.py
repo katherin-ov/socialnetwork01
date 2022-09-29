64019bb8305e1ba0a903b3ef635bdf3863e934d2
Катерина Новикова <katherinov.o@gmail.com>
2022-09-29 18:49:20 +0300

    The first one

---
+++ yatube/posts/views.py
@@ -0,0 +1,26 @@
+
+from .models import Post, Group
+from django.shortcuts import render, get_object_or_404
+
+
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+        'title': "Последние обновления на сайте",
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
