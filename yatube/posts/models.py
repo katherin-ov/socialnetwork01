7b98ad830c0a9e698128496ffd722f2ecbc2c408
Катерина Новикова <katherinov.o@gmail.com>
2022-09-30 18:56:39 +0300

    Second try

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -23,7 +23,10 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         related_name='posts',
         blank=True, null=True,
     )
+
+    class Meta:
+        ordering = ['-pub_date']
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,25 +2,20 @@
 from .models import Post, Group
 from django.shortcuts import render, get_object_or_404
 
+LIMIT = 10
+
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка
-    # из 10 объектов модели Post,
-    # отсортированных по полю pub_date
-    # по убыванию (от больших значений к меньшим)
-    posts = Post.objects.order_by('-pub_date')[:10]
-    # В словаре context отправляем информацию в шаблон
+    posts = Post.objects.all()[:LIMIT]
     context = {
         'posts': posts,
-        'title': "Последние обновления на сайте",
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:LIMIT]
     context = {
         'group': group,
         'posts': posts,
